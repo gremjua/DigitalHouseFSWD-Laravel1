cdae3f54d04d3479a5f3f9474a2852b5c156b6ef
Juan Gremes <gremjua@gmail.com>
2019-12-15 15:49:05 -0300

    Added movie_factory and actor_factory.

---
+++ database/factories/actor_factory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Actor;
+use Faker\Generator as Faker;
+
+$factory->define(Actor::class, function (Faker $faker) {
+    return [
+        //
+        $first_name => $faker->firstName(),
+        $last_name => $faker->lastName(),
+        $rating => $faker->numberBetween(1, 10)
+    ];
+});
---
+++ database/factories/movie_factory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Movie;
+use Faker\Generator as Faker;
+
+$factory->define(Movie::class, function (Faker $faker) {
+    return [
+        //
+        $title => $faker->word,
+        $rating => $faker->numberBetween(1,10),
+        $awards => $faker->numberBetween(0,15),
+        $release_date => $faker->date($format = 'Y-m-d', $max = 'now'),
+        $length => $faker->numberBetween(30,210)
+    ];
+});
